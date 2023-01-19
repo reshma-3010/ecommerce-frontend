f1e45e9f12600d0e61f4ae759e3bef52d3fd8ea4
Reshma <reshmadudekula90@gmail.com>
2023-01-19 19:49:05 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,57 @@
+const data = {
+  products: [
+    {
+      name: "Nike Slim shirt",
+      slug: "nike-slim-shirt",
+      category: "Shirts",
+      image:
+        "https://cdn.shopify.com/s/files/1/0752/6435/products/IMG_0127_9b29d079-aac3-46da-84b8-91b60243bdd7_765x.jpg?v=1661004175", // 679px × 829px
+      price: 120,
+      countInStock: 10,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality shirt",
+    },
+    {
+      name: "Adidas Fit Shirt",
+      slug: "adidas-fit-shirt",
+      category: "Shirts",
+      image:
+        "https://assets.ajio.com/medias/sys_master/root/20220420/SQg7/625f0c0aaeb26921af2bacdf/-473Wx593H-469131589-red-MODEL.jpg",
+      price: 250,
+      countInStock: 20,
+      brand: "Adidas",
+      rating: 4.0,
+      numReviews: 10,
+      description: "high quality product",
+    },
+    {
+      name: "Nike Slim Pant",
+      slug: "nike-slim-pant",
+      category: "Pants",
+      image:
+        "https://cdn.shopify.com/s/files/1/0261/7393/1605/products/SPORTONemanja00762_1728x.jpg?v=1667639985",
+      price: 25,
+      countInStock: 15,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 14,
+      description: "high quality product",
+    },
+    {
+      name: "Adidas Fit Pant",
+      slug: "adidas-fit-pant",
+      category: "Pants",
+      image:
+        "https://getketchadmin.getketch.com/product/8905404323927/660/HLTR004648_1.jpg",
+      price: 65,
+      countInStock: 5,
+      brand: "Puma",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality product",
+    },
+  ],
+};
+export default data;
